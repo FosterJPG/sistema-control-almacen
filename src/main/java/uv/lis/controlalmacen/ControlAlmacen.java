e32eba903c954472556aa6a0680f261e80c6debf
Isaac <zS24013258@estudiantes.uv.mx>
2026-05-07 10:46:25 -0600

    maquetación de pantallas FXML, CSS e imágenes

--- src/main/java/uv/lis/controlalmacen/ControlAlmacen.java
+++ src/main/java/uv/lis/controlalmacen/ControlAlmacen.java
@@ -1,6 +1,9 @@
 package uv.lis.controlalmacen;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ControlAlmacen extends Application {
@@ -8,6 +11,13 @@ public class ControlAlmacen extends Application {
     // El método start es obligatorio en JavaFX
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/InicioSesion.fxml"));
+        Scene scene = new Scene(root);
+
+        // Cargar el CSS
+        String css = this.getClass().getResource("/css/estilos.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        
         primaryStage.setTitle("Control de Almacén - GLOBAL FINANCE");
         primaryStage.show();
     }
