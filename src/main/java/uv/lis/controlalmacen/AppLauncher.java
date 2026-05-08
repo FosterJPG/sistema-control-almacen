8c31c51f56beda5a9c296c4e021b3b9402ca138a
sebas <barmoraseb@gmail.com>
2026-05-08 11:51:12 -0600

    Correcion en la versión del pom.xml

--- src/main/java/uv/lis/controlalmacen/AppLauncher.java
+++
@@ -1,8 +0,0 @@
-package uv.lis.controlalmacen;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        // Llama al método main de tu clase original de JavaFX
-        ControlAlmacen.main(args); 
-    }
-}
