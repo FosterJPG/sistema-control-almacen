ae9715fd5a50c83809c527e18f7aaa60185feb12
YaraOrtizOlmos <yaraortizolmos@gmail.com>
2026-05-21 14:06:09 -0600

    Navegación de items

--- src/main/java/uv/lis/controlalmacen/ControlAlmacen.java
+++ src/main/java/uv/lis/controlalmacen/ControlAlmacen.java
@@ -5,23 +5,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import uv.lis.controlalmacen.utilidades.UtilidadesFX;
+
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class ControlAlmacen extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/InicioSesion.fxml"));
+    public void start(Stage primaryStage){
+        try {
+            Parent root = UtilidadesFX.cargarFXML("InicioSesion");
+            Scene scene = new Scene(root);
 
-        Scene scene = new Scene(root);
+            primaryStage.setTitle("Control de Almacén - GLOBAL FINANCE");
+            primaryStage.setResizable(false);
+            primaryStage.centerOnScreen();
+            primaryStage.setScene(scene);
 
-        String css = this.getClass().getResource("/css/estilos.css").toExternalForm();
-        scene.getStylesheets().add(css);
+            String css = this.getClass().getResource("/css/estilos.css").toExternalForm();
+            scene.getStylesheets().add(css);
 
-        primaryStage.setTitle("Control de Almacén - GLOBAL FINANCE");
-        primaryStage.setScene(scene);
-        primaryStage.centerOnScreen();
-        primaryStage.show();
-        primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
