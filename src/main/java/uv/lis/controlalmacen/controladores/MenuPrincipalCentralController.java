96a0bc6138fe7dfa83a99b9efe6d96032b0bef99
YaraOrtizOlmos <yaraortizolmos@gmail.com>
2026-06-11 04:26:42 -0600

    CRUD Encargados

--- src/main/java/uv/lis/controlalmacen/controladores/MenuPrincipalCentralController.java
+++ src/main/java/uv/lis/controlalmacen/controladores/MenuPrincipalCentralController.java
@@ -44,7 +44,7 @@ public class MenuPrincipalCentralController implements Initializable, MenuContro
     }
 
     @FXML private void clicItems(ActionEvent e)      { navegarA("CatalogoItems",        "Catálogo de Ítems"); }
-    @FXML private void clicEncargados(ActionEvent e) { navegarA("GestorEmpleados",      "Gestión de Encargados"); }
+    @FXML private void clicEncargados(ActionEvent e) { navegarA("CatalogoEncargados",      "Gestión de Encargados"); }
     @FXML private void clicSucursales(ActionEvent e) { navegarA("CatalogoSucursales",   "Sucursales"); }
     @FXML private void clicPartidas(ActionEvent e)   { navegarA("CatalogoPartidas",     "Partidas Presupuestales"); }
     @FXML private void clicFacturas(ActionEvent e)   { navegarA("ListadoFacturas",      "Listado de Facturas"); }
